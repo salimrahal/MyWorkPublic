a0b7906ecbb0496d5b2a79c77a818f40628c6845
salim <salim@ss-host>
2014-08-14 15:40:42 +0300

    SiptoolServer: v0.1

---
+++ sip/SIPTOOLS/SipToolsServer/src/siptoolServer/MainServer.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package siptoolServer;
+
+import algechoserver.bo.EchoServerDatagram;
+import algechoserver.bo.EchoServerTcp;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import network.Networking;
+import trafficServer.bo.TrfThreadDgm;
+
+/**
+ *
+ * @author salim
+ */
+public class MainServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) throws SocketException, UnknownHostException {
+        //for remote test or on production
+        String localIp = Networking.getLocalIpAddress();//"127.0.1.1";//
+        //for local host test
+        localIp = "127.0.1.1";
+
+        /**
+         * ***********ALG Echo Server*********************
+         */
+        Integer defaultport = 5092;
+        String portStr;
+        if (args.length == 0) {
+            System.out.println("The server by default run on " + defaultport + ", you can change the port by passing it as parameter: example: java -jar AlgEchoServer.jar 5068."
+                    + "");
+            //launching UDP TCP echo server thread
+            launchingEchoServer(localIp, defaultport);
+        } else {
+            portStr = args[0];
+            Integer port = Integer.valueOf(portStr);
+            launchingEchoServer(localIp, port);
+
+        }
+
+        /**
+         * *********** Traffic Server It listens on five ports, accepts
+         * traffic(upload) from multiple clients, and generates download traffic
+         * depending on the codec or pps
+           *********************
+         */
+        Integer[] portArr;
+        Integer port1 = 5094;
+        Integer port2 = 5095;
+        Integer port3 = 5096;
+        Integer port4 = 5097;
+        Integer port5 = 5098;
+
+        portArr = new Integer[]{port1, port2, port3, port4, port5};
+        
+        launchingTrafficServer(localIp, portArr);
+    }
+
+    public static void launchingEchoServer(String localIp, Integer port) throws SocketException, UnknownHostException {
+        //launching UDP echo server thread
+        EchoServerDatagram serverUdp = new EchoServerDatagram(localIp, port);
+        Thread serverUdpThread = new Thread(serverUdp);
+        serverUdpThread.start();
+
+        //launching TCP echo server
+        EchoServerTcp serverTcp = new EchoServerTcp(port);
+        Thread serverTcpThread = new Thread(serverTcp);
+        serverTcpThread.start();
+
+    }
+
+    public static void launchingTrafficServer(String localIp, Integer[] portArr) throws SocketException, UnknownHostException {
+       System.out.println("Launching Traffic server...on ports: "
+                + portArr[0] + ", " + portArr[1] + ", " + portArr[2] + ", " + portArr[3] + ", " + portArr[4]);
+        //launching traffic UDP server
+        for (Integer port : portArr) {
+            TrfThreadDgm trfthread = new TrfThreadDgm(localIp, port);
+            Thread serverTrfThread = new Thread(trfthread);
+            serverTrfThread.start();
+        }
+    }
+
+}
---
+++ sip/SIPTOOLS/SipToolsServer/src/trafficServer/bo/TrfThreadDgm.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trafficServer.bo;
+
+import algechoserver.bo.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author salim
+ * trafficThread datagram
+ */
+public class TrfThreadDgm implements Runnable{
+
+    DatagramSocket socket = null;
+    BufferedReader in = null;
+    InetAddress address;
+    String registerKey = "REGISTER";
+    String inviteKey = "INVITE";
+    //// poolsize=30; unsused
+    Integer port;
+    Integer poolsize = 30 * Runtime.getRuntime().availableProcessors();
+
+
+    public TrfThreadDgm(String localIp, Integer port) throws SocketException, UnknownHostException {
+        this.port = port;
+        address = InetAddress.getByName(localIp);
+        socket = new DatagramSocket(port, address);
+        System.out.println("Traffic DatagramServer: listening on port: " + port + "/ Ip: " + localIp+" / use a cached pool");   
+    }
+    
+       @Override
+    public void run() {
+        try {
+            processrequests();
+        } catch (SocketException | UnknownHostException ex) {
+            System.out.println("Traffic DatagramServer:: Error: couldn't run the serverUdp:"+ex.getLocalizedMessage()); 
+        }
+    }
+
+    public void processrequests() throws UnknownHostException, SocketException {
+        int i = 0;
+        String subStr;
+        ExecutorService poolservice = Executors.newCachedThreadPool();
+        System.out.println("Traffic DatagramServer:(UDP Server) starts..port:"+ this.port);
+        //buffer to receive incoming data
+        byte[] buf = new byte[1024];//65536
+        try {
+
+            System.out.println("Traffic DatagramServer:: waiting to receive packets....port:"+ this.port);
+            //communication loop
+            while (true) {
+                //create udp packet
+                DatagramPacket incomingPacket = new DatagramPacket(buf, buf.length);
+                // receive request
+                socket.receive(incomingPacket);
+                //DatagramPacket incomingPacketTmp = incomingPacket;
+                String recvMsg = new String(incomingPacket.getData(), 0, incomingPacket.getLength());
+                System.out.println("["+new Date()+"]\n received packet clientID:" + i + "\n" + recvMsg);
+                //check the message type to ensure it's: register or invite, drop the sip spam
+                subStr = recvMsg.substring(0, 8);//check the first line or method type
+                    
+                //only echo the register and invite message
+                if (subStr.contains(registerKey)||subStr.contains(inviteKey)) {
+                    // send the response to the client at "address" and "port"
+                    InetAddress addressInco = incomingPacket.getAddress();
+                    int portInco = incomingPacket.getPort();
+                    
+                    //create the thread(Runnable) that sends the message                  
+                    ClientTrfDgmConnection clientConn = new ClientTrfDgmConnection(socket, recvMsg, addressInco, portInco, i);
+                    //and this task to a pool, so clientConnection thread will be started
+                    poolservice.execute(clientConn);
+                    i++;
+                } else {
+                    System.out.println("Traffic DatagramServer::unknown client, disregard the packet");
+                }
+
+            }//end of while
+        }//end try
+        catch (IOException e) {
+            System.out.println("Error:" + e.getLocalizedMessage());
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
+
+ 
+}
