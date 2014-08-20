142b0bee7600f144acebccc777c3175f24b85b70
salim <salim@ss-host>
2014-08-20 18:19:25 +0300

    SiptoolApp: v0.1

--- sip/SIPTOOLS/Alg_Echoclient_server/AlgPluginEcho/src/algConcurrent/SRC.java
+++ sip/SIPTOOLS/Alg_Echoclient_server/AlgPluginEcho/src/algConcurrent/SRC.java
@@ -14,43 +14,43 @@ import java.util.concurrent.Callable;
 
 /**
  *
- * @author salim
- * this task handle the tcp send and received message, it should be surrounded with timeout exception in 
- * case the ReadLine blocks
+ * @author salim this task handle the tcp send and received message, it should
+ * be surrounded with timeout exception in case the ReadLine blocks
  */
 public class SRC implements Callable<String> {
-String msgToSend;
-BufferedReader in; 
-PrintWriter out;
+
+    String msgToSend;
+    BufferedReader in;
+    PrintWriter out;
 
     public SRC(String msgToSend, BufferedReader in, PrintWriter out) {
         this.msgToSend = msgToSend;
         this.in = in;
         this.out = out;
     }
-    
+
     @Override
     public String call() throws Exception {
         System.out.println("call:: the task begins");
-         StringReader msgreader = new StringReader(msgToSend);
-            BufferedReader msgbr = new BufferedReader(msgreader);
-            String msgRecv;
-            String submsgToSend;
-            StringBuilder strbuilder = new StringBuilder();
-            boolean firstLine = true;
+        StringReader msgreader = new StringReader(msgToSend);
+        BufferedReader msgbr = new BufferedReader(msgreader);
+        String msgRecv;
+        String submsgToSend;
+        StringBuilder strbuilder = new StringBuilder();
+        boolean firstLine = true;
         while ((submsgToSend = msgbr.readLine()) != null) {
             //write to the server
             out.println(submsgToSend);
-                //recieve from the server,
+            //recieve from the server,
             //in some case it will freeze here nothing is received, so a timeout will be triggered
             msgRecv = in.readLine();
-            if(firstLine){
+            if (firstLine) {
                 System.out.println("echo: " + msgRecv);
                 firstLine = false;
             }
             strbuilder.append(msgRecv).append("\r\n");
         }
-    return strbuilder.toString();
+        return strbuilder.toString();
     }
 
 }
