87927da576b35a23cb95d15d3c8f109e8894d320
Salim Rahal <salim.rahal@isae.edu.lb>
2014-11-26 19:44:03 +0200

    glg api

--- googleAPI/DriveTest/src/drivetest/DriveCommandLine.java
+++ googleAPI/DriveTest/src/drivetest/DriveCommandLine.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package drivetest;
+
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
@@ -20,52 +21,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+
 /**
  *
- * @author salim
- * Google App link: https://console.developers.google.com/project/endless-theorem-771
+ * @author salim Google App link:
+ * email: salim.rahal@isae.edu.lb
+ * https://console.developers.google.com/project/spry-optics-774/apiui/credential?clientType&authuser=1 Google App
+ * Google App type: installed applications
+ * tutorial url: https://developers.google.com/drive/web/quickstart/quickstart-java
  */
-
-
-
 public class DriveCommandLine {
 
-  private static String CLIENT_ID = "1083142588001-hhj4srl2a34a1v0vadoomo0eiip5e57j.apps.googleusercontent.com";//you client ID
-  private static String CLIENT_SECRET = "ZUpcJygZrtIjleUlLefUI4Ef";//ypur client secret
+    private static String CLIENT_ID = "242113663098-08ilq6cqftbldjv32uevv3680742a0nc.apps.googleusercontent.com";//you client ID
+    private static String CLIENT_SECRET = "deoR20F73BODf-OA-UHoDZnS";//your client secret
 
-  private static String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
-  
-  public static void main(String[] args) throws IOException {
-    HttpTransport httpTransport = new NetHttpTransport();
-    JsonFactory jsonFactory = new JacksonFactory();
-   
-    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-        httpTransport, jsonFactory, CLIENT_ID, CLIENT_SECRET, Arrays.asList(DriveScopes.DRIVE))
-        .setAccessType("online")
-        .setApprovalPrompt("auto").build();
-    
-    String url = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
-    System.out.println("Please open the following URL in your browser then type the authorization code:");
-    System.out.println("  " + url);
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String code = br.readLine();
-    
-    GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
-    GoogleCredential credential = new GoogleCredential().setFromTokenResponse(response);
-    
-    //Create a new authorized API client
-    Drive service = new Drive.Builder(httpTransport, jsonFactory, credential).build();
+    private static String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
 
-    //Insert a file  
-    File body = new File();
-    body.setTitle("My document");
-    body.setDescription("A test document");
-    body.setMimeType("text/plain");
-    
-    java.io.File fileContent = new java.io.File("./src/drivetest/document.txt");
-    FileContent mediaContent = new FileContent("text/plain", fileContent);
+    public static void main(String[] args) throws IOException {
+        HttpTransport httpTransport = new NetHttpTransport();
+        JsonFactory jsonFactory = new JacksonFactory();
 
-    File file = service.files().insert(body, mediaContent).execute();
-    System.out.println("File ID: " + file.getId());
-  }
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, jsonFactory, CLIENT_ID, CLIENT_SECRET, Arrays.asList(DriveScopes.DRIVE))
+                .setAccessType("online")
+                .setApprovalPrompt("auto").build();
+
+        String url = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
+        System.out.println("Please open the following URL in your browser then type the authorization code:");
+        System.out.println("  " + url);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String code = br.readLine();
+
+        GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
+        GoogleCredential credential = new GoogleCredential().setFromTokenResponse(response);
+
+        //Create a new authorized API client
+        Drive service = new Drive.Builder(httpTransport, jsonFactory, credential).build();
+
+        //Insert a file
+        File body = new File();
+        body.setTitle("My document");
+        body.setDescription("A test document");
+        body.setMimeType("text/plain");
+
+        java.io.File fileContent = new java.io.File("./src/drivetest/document.txt");
+        FileContent mediaContent = new FileContent("text/plain", fileContent);
+
+        File file = service.files().insert(body, mediaContent).execute();
+        System.out.println("File ID: " + file.getId());
+
+        //use patch to update the file content: not completed
+//        java.io.File fileContentNew = new java.io.File("./src/drivetest/documentnew.txt");
+//        service.files().patch(file.getId(), fileContentNew);
+    }
+    
 }
