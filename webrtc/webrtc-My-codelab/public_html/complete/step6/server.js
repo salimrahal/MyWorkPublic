21ec10d475f1a4fea9833a99b0e9d910d76a9435
Salim Rahal <salim.rahal@isae.edu.lb>
2015-01-30 19:27:02 +0200

    webrtc

--- webrtc/webrtc-My-codelab/public_html/complete/step6/server.js
+++ webrtc/webrtc-My-codelab/public_html/complete/step6/server.js
@@ -1,13 +1,14 @@
 var static = require('node-static');
 var http = require('http');
 var file = new (static.Server)();
-var app = http.createServer(function(req, res) {
+var app = http.createServer(function (req, res) {
     file.serve(req, res);
 }).listen(2014);
 
+
 var io = require('socket.io').listen(app);
 
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
 
     // convenience function to log server messages on the client
     function log() {
@@ -18,13 +19,13 @@ io.sockets.on('connection', function(socket) {
         socket.emit('log', array);
     }
 
-    socket.on('message', function(message) {
+    socket.on('message', function (message) {
         log('Got message:', message);
         // for a real app, would be room only (not broadcast)
         socket.broadcast.emit('message', message);
     });
 
-    socket.on('create or join', function(room) {
+    socket.on('create or join', function (room) {
         //original code of getting clients number socket.io 0.9
         //var numClients = io.sockets.clients(room).length;
 
