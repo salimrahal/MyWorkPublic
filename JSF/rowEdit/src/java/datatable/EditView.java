730b68c460f7cd1085ba5d7f829b9c9a9648b2b6
salimrahal <salim.rahal101@gmail.com>
2015-03-05 21:06:04 +0200

    jsf

--- JSF/rowEdit/src/java/datatable/EditView.java
+++ JSF/rowEdit/src/java/datatable/EditView.java
@@ -22,20 +22,13 @@ import javax.faces.bean.ViewScoped;
 public class EditView implements Serializable {
 
     private List<CarD> cars1;
-    private List<CarD> cars2;
-    private List<Tank> tankL;
     private CarD selectedCar;
     @ManagedProperty("#{carServiceD}")
     private CarServiceD service;
 
-    @ManagedProperty("#{tankServiceD}")
-    private TankServiceD serviceTank;
-
     @PostConstruct
     public void init() {
         cars1 = service.createCars(10);
-        cars2 = service.createCars(10);
-        tankL = serviceTank.createTanks(10);
     }
 
     public void editerAction() {
@@ -43,15 +36,11 @@ public class EditView implements Serializable {
     }
 
     public void prepareEdit() {
-       // System.out.println("prepareEdit..");
+        // System.out.println("prepareEdit..");
     }
 
     public List<CarD> getCars1() {
         return cars1;
-    }
-
-    public List<CarD> getCars2() {
-        return cars2;
     }
 
     public List<String> getBrands() {
@@ -65,43 +54,13 @@ public class EditView implements Serializable {
     public void setService(CarServiceD service) {
         this.service = service;
     }
-    /*
-     Tank
-     */
-
-    public List<Tank> getTankL() {
-        return tankL;
-    }
-
-    public void setTankL(List<Tank> tankL) {
-        this.tankL = tankL;
-    }
-
-    public List<String> getCapacities() {
-        return serviceTank.getCapacities();
-    }
-
-    public void setServiceTank(TankServiceD serviceTank) {
-        this.serviceTank = serviceTank;
-    }
 
     public CarD getSelectedCar() {
-        if (selectedCar != null) {
-           // System.out.println("get selected car:" + selectedCar.getId());
-        } else {
-           // System.out.println(" selected car:null");
-        }
-
         return selectedCar;
     }
 
     public void setSelectedCar(CarD selectedCar) {
         this.selectedCar = selectedCar;
-        if (selectedCar != null) {
-           // System.out.println("setSelectedCar:" + selectedCar.getId());
-        } else {
-           // System.out.println(" setSelectedCar car:null");
-        }
     }
 
 }
