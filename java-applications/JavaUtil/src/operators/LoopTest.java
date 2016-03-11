924b367c5ce51225531e74aa01bef97303f1217e
salim <salim.rahal@isae.edu.lb>
2016-03-11 15:26:08 +0200

    outerloop

--- java-applications/JavaUtil/src/operators/LoopTest.java
+++ java-applications/JavaUtil/src/operators/LoopTest.java
@@ -6,6 +6,30 @@ public class LoopTest {
      * @param args
      */
     public static void main(String[] args) {
+        breakEmbededLoops();
+    }
+
+    public void loopTest() {
+        for (int i = 10, max = 11; i <= max; i++) {
+
+        }
+    }
+
+    public static void breakEmbededLoops() {
+        outerloop:
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i * j > 6) {
+                    System.out.println("Breaking: i * j ["+(i * j)+"]> 6: ");
+                    break outerloop;
+                }
+                System.out.println(i + " " + j);
+            }
+        }
+        System.out.println("Done");
+}
+
+public void doWhiletest() {
         // TODO Auto-generated method stub
         int count = 1;
         boolean moretask = true;
@@ -16,12 +40,6 @@ public class LoopTest {
                 moretask = false;
             }
         } while (moretask);
-    }
-
-    public void loopTest() {
-        for (int i = 10, max = 11; i <= max; i++) {
-
-        }
     }
 
 }
