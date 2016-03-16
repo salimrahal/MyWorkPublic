972579f850b2782f0c2be70445985e51657e78d5
salim <salim.rahal@isae.edu.lb>
2016-03-16 18:45:22 +0200

    lambda

--- java-applications/JavaUtil/src/lambda/list/TestList.java
+++ java-applications/JavaUtil/src/lambda/list/TestList.java
@@ -5,8 +5,10 @@
  */
 package lambda.list;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  *
@@ -15,19 +17,40 @@ import java.util.List;
 public class TestList {
 
     public static void main(String[] args) {
-        testListWithLE(); 
+        testListWithLE();
     }
-public static void testListWithLE(){
-      //Old way:
+
+    public static void testListWithLE() {
+        //Old way:
         List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
         for (Integer n : list1) {
             System.out.println(n);
         }
-        //new way
-        list1.forEach((e)->System.out.println("using LE:"+e));
-        
+        list1.forEach((e) -> System.out.println("using Lambda Exp:" + e));
+
         //or we can use :: double colon operator in Java 8
         list1.forEach(System.out::println);
 
-}
+    }
+
+    public static void testListWithLE_FlatFunction() {
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        list1.forEach((e) -> {
+            System.out.println("behh");
+            int x = 0;
+            x = x+e;
+        });
+    }
+
+    public static void testListWithLE_InnerFunction() {
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        list1.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer e) {
+                List<Integer> l = new ArrayList<>();
+                l.add(e);
+                System.out.println("Inner function addin " + e);
+            }
+        });
+    }
 }
