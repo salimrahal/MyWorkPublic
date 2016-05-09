63ab21e43444f4ab77c48b06ff2ed0c770d66d8e
salim <salim.rahal@isae.edu.lb>
2016-05-09 17:28:18 +0300

    email regex

---
+++ java-applications/JavaUtil/src/regex/EmailValidator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author salim
+ */
+public class EmailValidator {
+
+    public static void main(String[] args) {
+        String wrongEmail = "ssss";
+        String trueEmail = "ss@hotmail.com";
+        System.out.println(validate(trueEmail));
+    }
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX
+            = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
+    }
+}
--- shellscript/deploy/src/deploy/Deploy.java
+++ shellscript/deploy/src/deploy/Deploy.java
@@ -11,29 +11,44 @@ import java.io.InputStreamReader;
 
 /**
  *
- * @author salim
- * the 
+ * @author salim the
  */
 public class Deploy {
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         // TODO code application logic here
+        String absoluteDir = "/home/salim/Development/projets-siISAE/resources/dist/";
         String appToDeploy = "resources.war";
-        String scrpt = "./src/deploy/deployToDeploy.sh"+" "+appToDeploy;
+        String scrpt = "./src/deploy/deployToDeploy.sh" + " " + absoluteDir + appToDeploy;
         runSrcipt(scrpt);
     }
 
     public static void runSrcipt(String scrptName) {
         try {
             Process proc = Runtime.getRuntime().exec(scrptName); //Whatever you want to execute
+            System.out.println("exec done");
+
             BufferedReader read = new BufferedReader(new InputStreamReader(
                     proc.getInputStream()));
+
+            System.out.println("get input stream");
+
+            /*passing pssd not working                
+            System.out.println("write the pssd to outstream");
+               
+                             OutputStream procStdIn = proc.getOutputStream();
+                String pd = ">>>>>>";
+                byte[] pdb = pd.getBytes();
+                procStdIn.write(pdb);
+             */
             try {
+                System.out.println("waitfor");
                 proc.waitFor();
             } catch (InterruptedException e) {
                 System.out.println(e.getMessage());
             }
             while (read.ready()) {
+                System.out.println("inside read.ready()");
                 System.out.println(read.readLine());
             }
         } catch (IOException e) {
